f8c62900fe61852939f0a3b01db7ee09e992893f
Tejas parkhe <168290292+TejasParkhe-47@users.noreply.github.com>
2024-07-11 14:48:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+    distance : '80px',
+    duration : 2000,
+    delay: 200,
+})
+
+
+ScrollReveal().reveal('.home-content, heading', { origin : 'top'});
+ScrollReveal().reveal('.home-img, .service-container, .portfolio-box , .contact form', { origin : 'button'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin : 'left'});
+ScrollReveal().reveal('.home-content p , .about-content', { origin : 'right'});
+ScrollReveal().reveal('.aboutus h1 ', { origin : 'top'});
+ScrollReveal().reveal('.box1 ', { origin : 'left'});
+ScrollReveal().reveal('.box2 ', { origin : 'right'});
